3450f471c5e96044dcd6aa7c13ee5ddcab990dd1
Daniel <daniel.valdecantos@gmail.com>
2023-03-18 22:08:09 -0700

    chore: remove unused web server

--- src/index.js
+++ src/index.js
@@ -6,17 +6,6 @@ const {
 } = require("discord.js");
 const { scheduleAction, setCommands, interactions } = require("./utils");
 require("dotenv").config();
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(port, () => {
-  console.log(`app listening at http://localhost:${port}`);
-});
 
 const TOKEN = process.env.TOKEN;
 
