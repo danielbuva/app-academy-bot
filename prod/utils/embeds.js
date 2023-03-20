1aefa934867b027d5481be1aba859b33557b6f32
Daniel Valdecantos <70713396+danielbuva@users.noreply.github.com>
2023-03-19 20:09:28 -0700

    Delete prod directory
    accidentally committed: run npm run build for prod

--- prod/utils/embeds.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.embeds = void 0;
-const checkInLink = "\nhttps://progress.appacademy.io/me";
-exports.embeds = [
-    `check in                                 ☜(ﾟヮﾟ☜)${checkInLink}`,
-    ` ヽ༼ຈل͜ຈ༽_•︻ ┻̿═━一 ----o check in${checkInLink}`,
-];
